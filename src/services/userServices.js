4203747a3d91c345790dfd78840f5ee716e7ffab
amitjha25in <amit.jha@candere.com>
2025-08-12 17:05:13 +0530

    api

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 //const API_URL = process.env.REACT_APP_API_BASE_URL;
-const TOKEN = process.env.REACT_APP_API_TOKEN;
+//const TOKEN = process.env.REACT_APP_API_TOKEN;
 
 //Login API
 export const getLoginInfoByMobile = async (mobile) => {
