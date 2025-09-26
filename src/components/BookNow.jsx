86171a42aeb4e78ab8ce59280012e4730980cd0d
amitjha25in <amit.jha@candere.com>
2025-09-26 13:16:49 +0530

    test

--- src/components/BookNow.jsx
+++ src/components/BookNow.jsx
@@ -177,7 +177,7 @@ const BookNow = () => {
           )}
        
         {/* <Experties title="Timeless Care, Unmatched Expertise" data={expertiseData} item="4" /> */}
-        <div className="section-container" style={{"display":"none"}}>
+        <div className="section-container">
           <Heading title="Experience Pressto" />
           <div className="section-luxaryExperience-item">
             <div className="exp-item">
